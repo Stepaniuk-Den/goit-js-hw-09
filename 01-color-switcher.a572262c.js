585f609758eaa9a9ad0dcfb895e4748561cd8806
Stepaniuk-Den <128968908+Stepaniuk-Den@users.noreply.github.com>
2023-06-15 14:44:05 +0000

    Deploying to gh-pages from @ Stepaniuk-Den/goit-js-hw-09@7d0cefc301655f77ec89fe9cbafe2b5cced10cbd 🚀

---
+++ 01-color-switcher.a572262c.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,interval=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,clearInterval(interval)}))}();
+//# sourceMappingURL=01-color-switcher.a572262c.js.map
